aac00a79de697ec95b3c52ce52e2fb94bc7dc3de
TanyaFreebie <fjodorova.tanya@gmail.com>
2021-07-02 22:22:01 +0300

    Solving plan

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Assignment
+// Write a program that takes a value from the user and
+// prints out that number's position in fibonacci sequence.
+// Fibonacci sequence is a series of numbers where the previous two numbers sum up
+// to make the next number.
+// In this case, take the first two numbers of the sequence to be 1, 1
+// e.g. 1, 1, 2, 3, 5, 8, ...
+// Enter the position you want: 4
+// 3
+        //SOLUTION
+//request number from user
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of position in fibonacci sequence you want to observe: ");
+
+//start an array with that number
+        int[] number = new int[scanner.nextInt()];
+
+//write a loop with fibonacci sequence
+
+//print out answer
+        System.out.println("the number you are seeking is: " + );
+
+
+
+
+
+
+    }//End of void main
+}
